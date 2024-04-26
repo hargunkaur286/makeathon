648a1705b6edb0037064e55985451e66f250b98c
Hargun Kaur <hargunkaur286@gmail.com>
2024-04-27 00:10:00 +0530

    authentication

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,9 +1,51 @@
 import { Button } from "@/components/ui/button";
+import { Loader } from "lucide-react";
+import { ClerkLoading, ClerkLoaded, SignedIn, SignedOut, SignUpButton, SignInButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      This is a marketing page!
+    <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justigy-center p-4 gap-2">
+      <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
+        <Image src="/hero.png" fill alt="Hero" />
+      </div>
+      <div className="flex flex-col items-center gap-y-8">
+        <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
+          Play, Learn, and Prosper with Smart Money Choices
+        </h1>
+        <div>
+          <ClerkLoading>
+            <Loader className="h-5 w-5 text-muted-foregorund animate-spin" />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignedOut>
+
+              <SignUpButton role="modal" afterSignInUrl="/learn" afterSignUpUrl="/learn">
+                <Button size="lg" variant="secondary" className="w-full">
+                  Get Started
+                </Button>
+              </SignUpButton>
+
+              <SignInButton role="modal" afterSignInUrl="/learn" afterSignUpUrl="/learn">
+                <Button size="lg" variant="primaryOutline" className="w-full">
+                  I already have an account
+                </Button>
+              </SignInButton>
+
+            </SignedOut>
+            <SignedIn>
+              <Button size="lg" variant="secondary" className="w-full" asChild>
+                <Link href="/learn">
+                Continue Learning
+                </Link>
+                
+              </Button>
+            </SignedIn>
+          </ClerkLoaded>
+
+        </div>
+      </div>
     </div>
   );
 }
