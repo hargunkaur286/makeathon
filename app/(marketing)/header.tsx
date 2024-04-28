55693b4c62197893b7121819b998caa7ab579dde
Hargun Kaur <hargunkaur286@gmail.com>
2024-04-29 02:44:28 +0530

    sidebar

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,15 +1,15 @@
 import { Loader } from "lucide-react";
-import {ClerkLoading, ClerkLoaded, SignedIn, SignedOut, SignInButton, UserButton} from "@clerk/nextjs";
+import { ClerkLoading, ClerkLoaded, SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 
 export const Header = () => {
-    return(
+    return (
         <header className="h-20 w-full border-b-2 border-slate-200 px-4">
             <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
                 <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
-                <Image src="/logo.png" height={40} width={40} alt="logo" />
-                <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">FinEdge</h1>
+                    <Image src="/logo.png" height={40} width={40} alt="logo" />
+                    <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">FinEdge</h1>
                 </div>
                 <ClerkLoading>
                     <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
@@ -20,7 +20,7 @@ export const Header = () => {
                     </SignedIn>
                     <SignedOut>
                         <SignInButton mode="modal" afterSignInUrl="/learn" afterSignUpUrl="/learn">
-                        <Button size="lg" variant="ghost">Login</Button>
+                            <Button size="lg" variant="ghost">Login</Button>
                         </SignInButton>
                     </SignedOut>
                 </ClerkLoaded>
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,15 +1,43 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { SidebarItem } from "./sidebar-item";
+import {
+    ClerkLoading,
+    ClerkLoaded,
+    UserButton
+} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
 
 type Props = {
     className?: string;
 };
 
-export const Sidebar = ({className} : Props) => {
-    return(
-        <div className={cn("flex bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
-        className,
+export const Sidebar = ({ className }: Props) => {
+    return (
+        <div className={cn("flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+            className,
         )}>
-            Sidebar
+            <Link href="/learn">
+            <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+                <Image src="/logo.png" height={40} width={40} alt="logo" />
+                <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">FinEdge</h1>
+            </div>
+            </Link>
+            <div className="flex flex-col gap-y-2 flex-1">
+                <SidebarItem label="Learn" href="/learn" iconSrc="/learn.svg" />
+                <SidebarItem label="Leaderboard" href="/leaderboard" iconSrc="/leaderboard.svg" />
+                <SidebarItem label="Quests" href="/quests" iconSrc="/quest.svg" />
+                <SidebarItem label="Shop" href="/shop" iconSrc="/shop.svg" />
+            </div>
+            <div className="p-4">
+                <ClerkLoading>
+                    <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+                </ClerkLoading>
+                <ClerkLoaded>
+                    <UserButton afterSignOutUrl = "/" />
+                </ClerkLoaded>
+            </div>
         </div>
     )
 }
