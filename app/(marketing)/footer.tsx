390a363de51a648bf92f9ac9f07bccce1cfd6aca
Hargun Kaur <hargunkaur286@gmail.com>
2024-04-29 00:22:06 +0530

    footer

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,65 @@
+import { Button } from "@/components/ui/button"
+import Image from 'next/image'
+
 export const Footer = () => {
     return(
         <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-            Footer
+            <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image 
+                        src="/footer1.png" 
+                        alt="Financial Planning" 
+                        height={32} 
+                        width={40} 
+                        className="mr-4 rounded-md"
+                    />
+                    Planning
+                </Button>
+
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image 
+                        src="/footer2.png" 
+                        alt="Financial Planning" 
+                        height={32} 
+                        width={40} 
+                        className="mr-4 rounded-md"
+                    />
+                    Planning
+                </Button>
+
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image 
+                        src="/footer3.png" 
+                        alt="Financial Planning" 
+                        height={32} 
+                        width={40} 
+                        className="mr-4 rounded-md"
+                    />
+                    Planning
+                </Button>
+
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image 
+                        src="/footer4.png" 
+                        alt="Financial Planning" 
+                        height={32} 
+                        width={40} 
+                        className="mr-4 rounded-md"
+                    />
+                    Planning
+                </Button>
+
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image 
+                        src="/footer5.png" 
+                        alt="Financial Planning" 
+                        height={32} 
+                        width={40} 
+                        className="mr-4 rounded-md"
+                    />
+                    Planning
+                </Button>
+            </div>
         </footer>
     )
 }
