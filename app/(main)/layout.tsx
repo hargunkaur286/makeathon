bee9cb36d441dbe9f22c22b429431300d3558a7b
Hargun Kaur <hargunkaur286@gmail.com>
2024-04-29 16:34:49 +0530

    page wrappers

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -13,8 +13,10 @@ const MainLayout = ({
         <MobileHeader />
         <Sidebar className="hidden lg:flex" />
         <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
-            <div className="bg-red-500 h-full"></div>
+            <div className="h-full max-w-[1056px] mx-auto pt-6">
             {children}
+            </div>
+            
         </main>
         </>
     );
--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,22 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { UserProgress } from "@/components/user-progress";
+import { Header } from "./header";
+
 const LearnPage = () => {
     return(
-        <div>
-            Hello
+        <div className="flex flex-row-reverse gap-[48px] px-6">
+            <StickyWrapper>
+                <UserProgress
+                    activeCourse = {{ title: "Planning", imageSrc: '/footer1.png'}}
+                    hearts={5} 
+                    points={100}
+                    hasActiveSubscription={false}
+                />
+            </StickyWrapper>
+            <FeedWrapper>
+                <Header title="Planning" />
+            </FeedWrapper>
         </div>
     )
 }
